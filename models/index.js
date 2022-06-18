a953cee381e710afaed777b4922d2e59177792b6
Peter Tran <pettran866@gmail.com>
2022-06-17 20:38:36 -0700

    associations

---
+++ models/index.js
@@ -0,0 +1,36 @@
+// import all models
+const Post = require('./Post');
+const User = require('./User');
+const Comment = require('./Comment');
+
+// associations
+User.hasMany(Post, {
+  foreignKey: 'user_id'
+});
+
+Post.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'cascade'
+});
+
+User.hasMany(Comment, {
+  foreignKey: 'user_id',
+  onDelete: 'cascade'
+});
+  
+Comment.belongsTo(User, {
+  foreignKey: 'user_id',
+  onDelete: 'cascade'
+});
+
+Post.hasMany(Comment, {
+  foreignKey: 'post_id',
+  onDelete: 'cascade'
+});
+
+Comment.belongsTo(Post, {
+  foreignKey: 'post_id',
+  onDelete: 'cascade'
+});
+
+module.exports = { User, Post, Comment };
